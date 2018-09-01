ebed1a419f6e937fbf31691ba5f13b598a5f5284
Iftekhar Ahmed <Iftekhar.Ahmed@asos.com>
2018-09-01 21:37:05 +0100

    added tests cases

---
+++ RomanNumerals/RomanNumeralConverter.cs
@@ -0,0 +1,55 @@
+﻿namespace RomanNumerals
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class RomanNumeralConverter
+    {
+        private static readonly SortedList<int, char> RomanNumeralSymbolMeanings = new SortedList<int, char>
+        {
+            { 1, 'I' },
+            { 5, 'V' },
+            { 10, 'X' },
+            { 50, 'L' },
+            { 100, 'C' },
+            { 500, 'D' },
+            { 1000, 'M' },
+        };
+
+        public static string Convert(int numberToConvert)
+        {
+            var convertedNumeral = string.Empty;
+
+            foreach (var unit in RomanNumeralSymbolMeanings.Keys.Reverse())
+            {
+                var countOf = numberToConvert / unit;
+                var remainder = numberToConvert % unit;
+                var nextHighestUnit = RomanNumeralSymbolMeanings.Keys.IndexOf(unit) + 1;
+                var nextLowestUnit = RomanNumeralSymbolMeanings.Keys.IndexOf(unit) - 1;
+
+                if (countOf == 4)
+                {
+                    convertedNumeral =
+                        $"{RomanNumeralSymbolMeanings[unit]}{RomanNumeralSymbolMeanings.Values[nextHighestUnit]}";
+                    numberToConvert = remainder;
+                }
+                else if (countOf == 0 && remainder == 4)
+                {
+                    convertedNumeral =
+                        $"{RomanNumeralSymbolMeanings.Values[nextLowestUnit]}{RomanNumeralSymbolMeanings.Values[nextHighestUnit]}";
+                    numberToConvert = 0;
+                }
+                else if (countOf > 0 && countOf < 4)
+                {
+                    convertedNumeral = convertedNumeral + new string(
+                                           RomanNumeralSymbolMeanings[unit],
+                                           countOf);
+
+                    numberToConvert = remainder;
+                }
+            }
+
+            return convertedNumeral;
+        }
+    }
+}
--- RomanNumerals/RomanNumeralsTests.cs
+++ RomanNumerals/RomanNumeralsTests.cs
@@ -1,46 +1,29 @@
 ﻿namespace RomanNumerals
 {
-    using System.Collections.Generic;
-
     using NUnit.Framework;
 
     [TestFixture]
     public class RomanNumeralsTests
     {
-        [TestCase(1, "I")]
-        [TestCase(1, "II")]
-        [TestCase(1, "III")]
-        [TestCase(1, "IV")]
-        [TestCase(1, "V")]
-        [TestCase(1, "VI")]
-        [TestCase(1, "VII")]
-        [TestCase(1, "VIII")]
-        [TestCase(1, "IX")]
-        [TestCase(1, "X")]
-        public void GivenArabicNumberWhenConvertThenReturnRomanNumerals(int arabicNumberToConvert, string expectedRomanNumerals)
+        //[TestCase(1, "I")]
+        //[TestCase(2, "II")]
+        //[TestCase(3, "III")]
+        [TestCase(4, "IV")]
+        //[TestCase(5, "V")]
+        //[TestCase(6, "VI")]
+        //[TestCase(7, "VII")]
+        //[TestCase(8, "VIII")]
+        //[TestCase(9, "IX")]
+        //[TestCase(525, "DXXV")]
+        //[TestCase(727, "DCCXXVII")]
+        //[TestCase(3322, "MMMCCCXXII")]
+        public void GivenAnArabicNumberWhenConvertThenReturnRomanNumeral(
+            int numberToConvert,
+            string expectedRomanNumeral)
         {
-            var convertedNumber = RomanNumeralsConverter.Convert(arabicNumberToConvert);
-            Assert.That(convertedNumber, Is.EqualTo(expectedRomanNumerals));
-        }
-    }
+            var result = RomanNumeralConverter.Convert(numberToConvert);
 
-    public class RomanNumeralsConverter
-    {
-        private SortedList<int, char> romanNumeralDigits = new SortedList<int, char>
-            {
-                { 1, 'I' },
-                { 5, 'V' },
-                { 10, 'X' },
-                { 50, 'L' },
-                { 100, 'C' },
-                { 500, 'D' },
-                { 1000, 'M' }
-            };
-
-        public static string Convert(int arabicNumberToConvert)
-        {
-
-            return "I";
+            Assert.That(result, Is.EqualTo(expectedRomanNumeral));
         }
     }
 }
