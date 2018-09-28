93ba472762a474249396e324e53bb70139a12c1e
Iftekhar Ahmed <Iftekhar.Ahmed@asos.com>
2018-09-28 16:20:23 +0100

    Adds CodeWars solution

---
+++ CodeWars/OddInt.cs
@@ -0,0 +1,14 @@
+﻿namespace CodeWars
+{
+    using System.Linq;
+
+    public class OddInt
+    {
+        public static int FindIt(int[] integers)
+        {
+            var distinctOddNumbers = integers.Where(x => x % 2 > 0).Distinct();
+
+            integers.Select(x => )
+        }
+    }
+}
---
+++ CodeWarsTests/OddIntTests.cs
@@ -0,0 +1,15 @@
+﻿namespace CodeWarsTests
+{
+    using CodeWars;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class OddIntTests
+    {
+        [Test]
+        public void Tests()
+        {
+            Assert.AreEqual(5, OddInt.FindIt(new[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 }));
+        }
+    }
+}
